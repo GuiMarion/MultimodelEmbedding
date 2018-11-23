a12620cbd7b4fd3c13bd94d0fbbfc4432cef9526
Gui <33119610+GuiMarion@users.noreply.github.com>
2018-11-23 13:40:43 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+'''
+This file is used to plot all the functions we want in a same file.
+'''
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+X = np.arange(0, 100, 1)
+
+linear = np.copy(X)
+
+plt.plot(X, linear, label="y = x")
+plt.legend()
+plt.show()
