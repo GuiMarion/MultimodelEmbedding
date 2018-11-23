2fb0f2128ad61fd0d7c35c97a7fb1f5fb9afb7aa
felix <felix.rohrlich@lpp.polytechnique.fr>
2018-11-23 14:11:22 +0100

    commitGamma

--- main.py
+++ main.py
@@ -3,11 +3,23 @@ This file is used to plot all the functions we want in a same file.
 '''
 import matplotlib.pyplot as plt
 import numpy as np
+    
+def fact(n):
+    if n <= 0 :
+        return 1
+    else :
+        return n*fact(n-1)
+   
+def G(n):
+    return fact(n-1)
 
+def Gamma(n):
+    X = np.arange(0, n, 1.)
+    return [G(x) for x in X]
 
-X = np.arange(0, 100, 1)
-
-linear = np.copy(X)
+n = 100
+linear = np.log(Gamma(n))
+X = np.arange(0, n, 1)
 
 plt.plot(X, linear, label="y = x")
 plt.legend()
