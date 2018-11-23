ea2c1424456c7b302d94d78733a4e4bb24108b26
felix <felix.rohrlich@lpp.polytechnique.fr>
2018-11-23 18:55:30 +0100

    lala

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ This file is used to plot all the functions we want in a same file.
 '''
 import matplotlib.pyplot as plt
 import numpy as np
+import felix as fx
 
 X = np.arange(0, 100, 1)
 linear = np.copy(X)
 plt.figure();
 plt.plot(X, linear, label="y = x")
+plt.plot(X, fx.felix(100), label="Fonction Gamma")
 plt.legend()
 plt.show()
