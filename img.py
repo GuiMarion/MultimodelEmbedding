569858ca8a3b1d187a06aa1b94473380b7b3bc01
Gui <33119610+GuiMarion@users.noreply.github.com>
2018-11-25 20:57:17 +0100

    correction

--- img.py
+++ img.py
@@ -1,5 +1,6 @@
 import numpy as np
 from PIL import Image
+import os
 
 class img:
 	def __init__(self, path, matrix = False):
@@ -48,36 +49,62 @@ class img:
 		'''
 		(r, v, b) = val
 
-		#### TODO ####
+		m1, m2, m3 = self.mean()
 
-		return 
+		return np.sqrt((r-m1)**2 +  (v-m2)**2 + (b-m2)**2)
 
 	def getSubSquares(self, n):
 		'''
-		Should return subsquared of size n*n as a liste of img objects
+		Should return subsquared images of size n*n as a liste of img objects
 		'''
 
-		#### TODO ####
+		images = []
 
-		return
+		for i in range(len(self.matrix) // n - 1):
+			for j in range(len(self.matrix[0]) // n - 1):
+				images.append(img(self.matrix[i*n : (i+1)*n, j*n : (j+1)*n], matrix = True))
 
-def constructDataBase(path):
+		return images
+
+	def findClosestPoint(self, space):
+		dist =  100000
+		for elem in space:
+			distTemp = self.computeDistance(elem)
+			if distTemp <= dist:
+				dist = distTemp
+				returnedValue = elem
+
+		return returnedValue
+
+def constructDataBase(path, n):
 	'''
 	Open all the files from the path and return a list of all the resulting subsquares
 	'''
 
-	#### TODO ####
+	files = [os.path.join(path, f) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
 
-	return 
+	images = []
 
-def sortImages(images):
+	for file in files:
+		images.extend(img(file).getSubSquares(n))
+
+	return images
+
+def sortImages(images, OUT, spaceSize = 5):
 	'''
-	For every image in images, find the closest point of the dicretized space and save the image in the directory with the name of this point
+	For each image in images, find the closest point of the dicretized space and save the image in the directory with the name of this point
 	'''
+	space = discretizeSpace(spaceSize)
+	k = 0
+	for image in images:
 
-	#### TODO ####
+		point = image.findClosestPoint(space)
 
-	return
+		if not os.path.exists(OUT + "/" + str(point)):
+		    os.makedirs(OUT + "/" + str(point))
+
+		image.save(OUT + "/" + str(point) + "/" + str(k))
+		k += 1
 
 
 def discretizeSpace(n):
@@ -94,23 +121,17 @@ def discretizeSpace(n):
 
 	return points
 
-def findClosestPoint(space, value):
-	r,v,b = value
-	dist =  100000
-	for elem in space:
-		r2, v2, b2 = elem
-		distTemp = np.sqrt((r-r2)**2 + (v-v2)**2 + (b-b2)**2)
-		if distTemp <= dist:
-			dist = distTemp
-			returnedValue = elem
-
-	return returnedValue
 
 
 def main():
 	'''
 	Do the job if all the functions are working correctly
 	'''
-	data = constructDataBase("Data")
-	sortImages(data)
+	n = 10
+	DATA = "Data/"
+	OUT = "OUT/"+DATA
+	spaceSize = 5
+	data = constructDataBase(DATA, n)
+	sortImages(data, OUT, spaceSize = 5)
 
+main()
