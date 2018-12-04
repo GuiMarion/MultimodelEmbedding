079a4b3b5f65c47524961b9ea4cf9a36deb00c74
Clément Le Moine <Schenkel@MacBook-Pro-de-Clement.local>
2018-12-05 00:22:11 +0100

    score

--- Modules/score.py
+++ Modules/score.py
@@ -1,7 +1,10 @@
-from Modules import waveForm
+#from Modules import waveForm
+from music21 import converter
+import math
+import numpy as np
 
 class score:
-	def __init__(self, pahtToMidi, velocity=False):
+	def __init__(self, pathToMidi, velocity=False):
 
 		# Attributs to fill from the file
 		self.name = ""
@@ -10,18 +13,64 @@ class score:
 		self.pianoRoll = ""
 		self.instrument = ""
 		self.lenght = 0
+		#convert into a music21 stream
+		self.piece = converter.parse(pathToMidi) #convert to music 21 stream
+
+		
+
 
 	def computePianoRoll(self, velocity=False):
 
-		return "TODO"
+		piece = self.piece	#extract piece music21 stream from score 
+		quantization = 16  #set the quantization number of notes / s
+
+		# Extract the parts from the piece and compute piano roll on for each part
+		all_parts = {}
+		for part in piece.parts:
+			print(part)
+			try:
+				track_name = part[0].bestName()
+			except AttributeError:
+				track_name = 'None'
+
+		
+			# Get the measure offsets
+			measure_offset = {None:0}
+			for el in part.recurse(classFilter=('Measure')):
+				measure_offset[el.measureNumber] = el.offset
+			# Get the measure offsets
+			measure_offset = {None:0}
+			for el in part.recurse(classFilter=('Measure')):
+				measure_offset[el.measureNumber] = el.offset
+			# Get the duration of the part
+			duration_max = 0
+			for el in part.recurse(classFilter=('Note','Rest')):
+				t_end = int(math.ceil(((measure_offset[el.measureNumber] or 0) + el.offset + el.duration.quarterLength)*quantization))
+				if(t_end>duration_max):
+					duration_max=t_end
+			# Get the pitch and offset+duration
+			piano_roll_part = np.zeros((128,math.ceil(duration_max)))
+
+			for this_note in part.recurse(classFilter=('Note')):
+				note_start = int(math.ceil(((measure_offset[this_note.measureNumber] or 0) + this_note.offset)*quantization))
+				note_end = int(math.ceil(((measure_offset[this_note.measureNumber] or 0) + this_note.offset + this_note.duration.quarterLength)*quantization))
+				piano_roll_part[int(this_note.pitch.ps),note_start:note_end] = 1
+			#
+			cur_part = piano_roll_part;
+			if (cur_part.shape[1] > 0):
+				all_parts[track_name] = cur_part;
+
+			return piece, all_parts
 
 	def getPianoRoll(self):
 		# return the np.array containing the pianoRoll
-		return "TODO"
+		piece, all_parts = computePianoRoll(self,velocity=False)
+		return all_parts
 
 	def plot(self):
 		# plot the pianoRool representation
-		return "TODO"
+		piece, all_parts = computePianoRoll(self,velocity=False)
+		return piece.plot()
 
 	def toWaveForm(self, sound="default"):
 
@@ -32,3 +81,7 @@ class score:
 
 		# should return a temporal part of pianoroll as a score object
 		return score("")
+
+SCORE = score("John_Lennon_Imagine.mid",velocity=False)
+#piece, all_parts = SCORE.computePianoRoll(velocity=False)
+#piece.plot()
