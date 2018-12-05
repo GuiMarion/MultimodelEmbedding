ca3a757b3687f808416123223fd4a6de4e0eeb92
gdias94 <gabriel.dias.neto@gmail.com>
2018-12-05 08:08:55 +0100

    Ecrasement de la branche

---
+++ Modules/waveForm.py
@@ -0,0 +1,41 @@
+class waveForm:
+	def __init__(self, path):
+
+		if path is not None:
+			self.loadFromFile(path)
+
+		self.name = ""
+		self.tempo = 0
+		self.sampleRate = 0
+		self.data = []
+		
+	def loadFromFile(self, path):
+		# update the attributes, should use loadFrom data
+		return "TODO"
+
+	def loadFromData(self, sequence):
+		# update the attributes
+		# input sequence is a vector of samples
+		# should work with mono as well as with stereo 
+		return "TODO"
+
+	def play(self):
+		# play the data, use the module sound device
+		return "TODO"
+
+	def save(self, path):
+		# save as a wav file at path
+		return "TODO"
+
+	def plot(self):
+		# plot the signal
+		return "TODO"
+
+	def getFFT(self):
+		# return FFT if already computed or compute it, store it and return it
+		# store the result of the FFT as attribute in order to compute it only once
+		return "TODO"
+
+	def plotFFT(self):
+		# plot FFT
+		return "TODO"
