2a1c56e1e048a38bf7ef583c50cc4bb75946a29e
Gui <33119610+GuiMarion@users.noreply.github.com>
2019-01-03 01:18:42 +0100

    modifs

--- Exemples/score_Exemples.py
+++ Exemples/score_Exemples.py
@@ -5,7 +5,9 @@ from Modules import score
 from Modules import waveForm
 
 # Import one of my masterpieces ...
-s = score.score("Gui's_song.mid")
+s = score.score(pathToMidi="Gui's_score.mid")
+
+print(s.length)
 
 # Plot the piano roll representation of the score
 s.plot()
@@ -13,9 +15,13 @@ s.plot()
 # print the pianoRoll matrix
 print(s.getPianoRoll())
 
+print(s.getPianoRoll().shape)
+
 # plot the 10th first beats
 sub = s.extractPart(0, 10)
 sub.plot()
+
+quit()
 
 # play the waveform of the piece
 w = s.toWaveForm()
