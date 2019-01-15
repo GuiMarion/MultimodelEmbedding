d47f1db2f28e06ea66be0eac4267477454f93681
Gui <33119610+GuiMarion@users.noreply.github.com>
2019-01-15 19:27:48 +0100

    ok

--- WaveToMidi.py
+++ WaveToMidi.py
@@ -31,7 +31,7 @@ def getMidiFromWave(folder, file):
 
 	CQTs = []
 	for i in range(len(w[0])//172 -1):
-		CQTs.append(:, w[i*window_size : (i+1)*window_size])
+		CQTs.append(w[ : , i*window_size : (i+1)*window_size])
 
 	wraps = []
 	for elem in CQTs:
