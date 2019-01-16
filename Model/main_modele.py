2947f7f7e402673c49ef95543c2c912619eec2aa
Guilhem Marion <marion@laphroaig.ircam.fr>
2019-01-16 04:00:44 +0100

    ok

--- Model/main_modele.py
+++ Model/main_modele.py
@@ -6,8 +6,8 @@ from Model import modele
 import torch
 
 
-EPOCHS = 50
+EPOCHS = 35
 
-model = modele.Modele("../DataBase/MIDIs/mozart", gpu=1)
+model = modele.Modele("../DataBase/MIDIs/mozart", gpu=0)
 
 model.learn(EPOCHS, learning_rate=1e-2)
