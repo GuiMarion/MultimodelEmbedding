9002b8856f9b87fac3c2f6a70f747e7e1f7c019c
Gui <33119610+GuiMarion@users.noreply.github.com>
2019-01-22 13:24:56 +0100

    dependencies script

---
+++ dependencies.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python3 -m pip install numpy
+python3 -m pip install torch torchvision
+python3 -m pip install pypianoroll
+python3 -m pip install midi2audio
+python3 -m pip install librosa
+python3 -m pip install tqdm
+python3 -m pip install soundfile
+python3 -m pip install sounddevice
+python3 -m pip install matplotlib
